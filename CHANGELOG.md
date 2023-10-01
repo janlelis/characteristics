dcbff0f25c4c634759fc2d12a79a918e8842aa3c
Jan Lelis <hi@ruby.consulting>
2023-10-01 20:57:33 +0200

    Release v1.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 ## CHANGELOG
 
-### 1.6.0 (unreleased)
+### 1.6.0
 
 * Unicode 15.1 (general categories)
 
--- lib/characteristics/version.rb
+++ lib/characteristics/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Characteristics
-  VERSION = "1.5.0"
+  VERSION = "1.6.0"
   UNICODE_VERSION = "15.1.0"
 end
